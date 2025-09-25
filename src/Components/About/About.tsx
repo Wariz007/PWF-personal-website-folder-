4e1ae70d994a3b4cf455786e51dde72c68af43c1
wariz <bookbodmas31@gmail.com>
2025-09-25 02:15:27 +0100

    made finishing touches to the website before launch

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -9,9 +9,28 @@ function About() {
                 <h1>Olusanya Olabode <br />Abdulwariz</h1>
                 <p>This website is a collection of Wariz thoughts and opinions on how technology, entrepreneurship, and economics can contribute to the prosperity of the Federal Republic of Nigeria.</p>
                 <div className='contact-links'>
-                    <a href="#"><img src={Email} alt="Email" /></a>
-                    <a href="#"><img src={X} alt="Close" /></a>
-                    <a href="#"><img src={Github} alt="GitHub" /></a>
+                    <a 
+                        href="https://mail.google.com/mail/?view=cm&to=olusanyaolabodeabdulwariz@gmail.com"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <img src={Email} alt="Email" />
+                    </a>
+
+
+                    <a 
+                        href="https://x.com/Abdulwariz_"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <img src={X} alt="x formerly known as twitter" /></a>
+                    <a 
+                        href="https://github.com/Wariz007"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        >
+                        <img src={Github} alt="GitHub" />
+                    </a>
                 </div>
             </section>
         </>
