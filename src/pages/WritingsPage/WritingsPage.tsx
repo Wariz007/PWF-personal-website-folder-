9535d0732db536f8d8be58cdfb8b46b7b302d1c1
wariz <bookbodmas31@gmail.com>
2025-11-09 10:02:56 +0100

    update backend with latest fixes

--- src/pages/WritingsPage/WritingsPage.tsx
+++ src/pages/WritingsPage/WritingsPage.tsx
@@ -21,7 +21,7 @@ function WritingsPage() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch("http://localhost:5000/api/writings")
+    fetch("http://13.61.21.78:5000/api/writings")
       .then((res) => res.json())
       .then((data: Writing[]) => {
         console.log("Fetched writings:", data);
@@ -53,7 +53,7 @@ function WritingsPage() {
       {writing.image && (
         <div className="writing-image">
           <img
-            src={`http://localhost:5000${writing.image}`}
+            src={`http://13.61.21.78:5000${writing.image}`}
             alt={writing.title}
           />
         </div>
