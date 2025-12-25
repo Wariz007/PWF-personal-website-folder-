c3cff98e1062758443426daefa181750fbdd792b
wariz <bookbodmas31@gmail.com>
2025-12-25 22:13:01 +0100

    updated main.tsx and vite with git url

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  //base: '/PWF-personal-website-folder-/',  // <--- add this line
-  base: '/',
+  base: '/PWF-personal-website-folder-/',
+  //base: '/',
   plugins: [react()],
 })
