b238bc3ac8c2d7f2af9628ef8589627abaac1d5c
wariz <bookbodmas31@gmail.com>
2025-12-26 13:26:05 +0100

    added CNAME and updated fetch url

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/PWF-personal-website-folder-/',
-  //base: '/',
+  //base: '/PWF-personal-website-folder-/',
+  base: '/',
   plugins: [react()],
 })
